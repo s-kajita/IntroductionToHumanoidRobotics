6fee22957d57c69202bc309305cc5d8dd1cb7279
Shuuji Kajita <s.kajita@aist.go.jp>
2019-07-26 20:34:15 +0900

    Variable names were changed

--- AdjointMatrix.m
+++ AdjointMatrix.m
@@ -1,22 +1,20 @@
 function adjM = AdjointMatrix(M)
 % calculate Adjoint (Adjugate) matrix adjM where M^(-1) = det(M)*adjM
-% M must be a square matrix.
 
 Nrow = size(M,1);
 Ncol = size(M,2);
-
 adjM = zeros(Nrow,Ncol);
 
 if Ncol ~= Nrow
     fprintf('ERROR: Matrix must be square\n');
 else
-    for n=1:Ncol
-        Mn = M;
-        Mn(n,:) = [];        % Submatrix of M without row-n
+    for j=1:Ncol
+        Mj = M;
+        Mj(j,:) = [];        % Submatrix of M without row-j
         for k=1:Nrow
-            Mnk = Mn;
-            Mnk(:,k) = [];   % Submatrix of M without row-n and column-k 
-            adjM(k,n) = (-1)^(n+k)*det(Mnk);
+            Mjk = Mj;
+            Mjk(:,k) = [];   % Submatrix of M without row-j and column-k 
+            adjM(k,j) = (-1)^(j+k)*det(Mjk);
         end
     end
 end
